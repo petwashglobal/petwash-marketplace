63fbd59805ab880754e906474bafefe69b2b0113
petwashglobal <support@petwash.co.il>
2026-01-26 00:49:28 +1100

    Remove dist folder - let CI build fresh with Academy

--- dist/public/assets/BarChart-C0t_9D9f.js
+++
@@ -1,1 +0,0 @@
-import{g as a,B as i,f as s}from"./generateCategoricalChart-B_r4-Mgp.js";import{X as t,Y as r}from"./YAxis-XH1u4xyq.js";var p=a({chartName:"BarChart",GraphicalChild:i,defaultTooltipEventType:"axis",validateTooltipEventTypes:["axis","item"],axisComponents:[{axisType:"xAxis",AxisComp:t},{axisType:"yAxis",AxisComp:r}],formatAxisMap:s});export{p as B};
